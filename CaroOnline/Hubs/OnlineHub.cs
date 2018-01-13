15e5547b8a5711f3855a1cd083b4cfa682124683
VanHC <VanHC@DESKTOP-6DK60H1>
2018-01-13 16:01:17 +0700

    add SignalR and OnlineHub

---
+++ CaroOnline/Hubs/OnlineHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace CaroOnline.Hubs
+{
+    public class OnlineHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
