0b6cbb6a907dc20b8974ee18d7db6631d72cbe56
VanHC <VanHC@DESKTOP-6DK60H1>
2018-01-13 16:39:37 +0700

    fix connection to signal in onlinehub

--- CaroOnline/Hubs/OnlineHub.cs
+++ CaroOnline/Hubs/OnlineHub.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
-
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
 namespace CaroOnline.Hubs
 {
     public class OnlineHub : Hub
