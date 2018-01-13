22925aef75809935f2d52201970f4ee2097bcde2
VanHC <VanHC@DESKTOP-6DK60H1>
2018-01-13 15:01:05 +0700

    change layout, font

---
+++ CaroOnline/Scripts/Caro/Cell.js
@@ -0,0 +1,13 @@
+﻿class Cell {
+
+    constructor(x, y, Owner) {
+        this.x = x;
+        this.y = y;
+        this.Owner = Owner;
+    }
+    static SizeRect() {
+        return 30;//width=height=30
+    }
+    
+}
+
